14c2327276cbc26853aa5f0edaf9a495db0c22bb
Sam Sharff <samanthaleec@gmail.com>
2022-05-24 12:45:05 -0400

    creates calculator.vue component and imports to homeview.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Calculator msg="Calculator" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Calculator from "@/components/Calculator.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    Calculator,
   },
 };
 </script>
