74d55fd15d67598e967eae01d46e2b2ad9145f6a
Sam Sharff <samanthaleec@gmail.com>
2022-05-26 10:49:31 -0400

    adds division, multiplication, and subtractions methods to calculator; removes Vue logo and About.vue page

--- src/components/CalculatorView.vue
+++ src/components/CalculatorView.vue
@@ -9,15 +9,15 @@
       <div @click="append(8)" class="btn">8</div>
       <div @click="append(9)" class="btn">9</div>
 
-      <div class="btn operator">/</div>
+      <div @click="division" class="btn operator">/</div>
       <div @click="append(4)" class="btn">4</div>
       <div @click="append(5)" class="btn">5</div>
       <div @click="append(6)" class="btn">6</div>
-      <div class="btn operator">x</div>
+      <div @click="multiplication" class="btn operator">x</div>
       <div @click="append(1)" class="btn">1</div>
       <div @click="append(2)" class="btn">2</div>
       <div @click="append(3)" class="btn">3</div>
-      <div class="btn operator">-</div>
+      <div @click="subtraction" class="btn operator">-</div>
       <div @click="append(0)" class="btn">0</div>
       <div class="btn">.</div>
       <div @click="equal" class="btn equal">=</div>
@@ -55,6 +55,18 @@ export default {
     },
     addition() {
       this.operator = (a, b) => a + b;
+      this.setPrevNum();
+    },
+    subtraction() {
+      this.operator = (a, b) => a - b;
+      this.setPrevNum();
+    },
+    multiplication() {
+      this.operator = (a, b) => a * b;
+      this.setPrevNum();
+    },
+    division() {
+      this.operator = (a, b) => a / b;
       this.setPrevNum();
     },
     equal() {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <Calculator msg="Calculator" />
   </div>
 </template>
